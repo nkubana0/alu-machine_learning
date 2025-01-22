7fca2ef8dc67a37391104a017207f8023218acc2
shemankubana <shemaivan27@gmail.com>
2025-01-22 19:52:22 +0200

    4-line_up

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module 5-across_the_planes
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2):
+        return None
+    
+    for row1, row2 in zip(mat1, mat2):
+        if len(row1) != len(row2):
+            return None
+    result = []
+
+    for i in range(len(mat1)):
+        row_sum = [mat1[i][j] + mat2[i][j] for j in range(len(mat1[i]))]
+        result.append(row_sum)
+     
+    return result
