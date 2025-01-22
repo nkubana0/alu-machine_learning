c20d4a1f73ab84293eef4bdb62e0b30134840117
shemankubana <shemaivan27@gmail.com>
2025-01-22 19:20:22 +0200

    4-line_up

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module 4-line_up
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Adds two matrices element-wise.
+    """
+    if len(arr1) != len(arr2):
+        return None
+    
+    result = []
+
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+
+    return result
