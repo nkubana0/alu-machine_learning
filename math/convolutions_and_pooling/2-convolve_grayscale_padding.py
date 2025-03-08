cc117618fd7233396e7d189d6ab7631d6fddeacb
nkubana0 <i.shema@alustudent.com>
2025-03-08 16:25:14 +0200

    Add grayscale convolution with padding and example usage

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python3
+#!usr/bin/env python3
 """
 Function that performs a same convolution on grayscale images
 """
 import numpy as np
 
 
-def convolve_grayscale_same(images, kernel):
+def convolve_grayscale_padding(images, kernel, padding):
     """
     Function that performs a same convolution on grayscale images
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    pad_h, pad_w = kh // 2, kw // 2
+    ph, pw = padding
     padded_images = np.pad(
-        images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), mode='constant')
-    output = np.zeros((m, h, w))
+        images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    new_h, new_w = h + 2 * ph - kh + 1, w + 2 * pw - kw + 1
+    output = np.zeros((m, new_h, new_w))
 
-    for i in range(h):
-        for j in range(w):
+    for i in range(new_h):
+        for j in range(new_w):
             output[:, i, j] = np.sum(
                 padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
 
