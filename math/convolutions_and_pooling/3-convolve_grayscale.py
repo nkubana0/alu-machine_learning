e082140e4c5bd0d5ffa7a4c33be933b069a4a0a3
nkubana0 <i.shema@alustudent.com>
2025-03-08 16:33:04 +0200

    Add convolution functions for grayscale and multi-channel images with example usage

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Function that performs a convolution on grayscale images
+"""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Function that performs a convolution on grayscale images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2
+        pw = ((w - 1) * sw + kw - w) // 2
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded_images = np.pad(
+        images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    new_h = (h + 2 * ph - kh) // sh + 1
+    new_w = (w + 2 * pw - kw) // sw + 1
+    output = np.zeros((m, new_h, new_w))
+
+    for i in range(new_h):
+        for j in range(new_w):
+            output[:, i, j] = np.sum(
+                padded_images[:, i * sh:i * sh + kh, j * sw:j * sw + kw] * kernel, axis=(1, 2))
+
+    return output
---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Function that performs pooling on images
+"""
+import numpy as np
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """
+    Function that performs pooling on images
+    """
+    m, h, w, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    new_h = (h - kh) // sh + 1
+    new_w = (w - kw) // sw + 1
+    output = np.zeros((m, new_h, new_w, c))
+
+    for i in range(new_h):
+        for j in range(new_w):
+            if mode == 'max':
+                output[:, i, j, :] = np.max(
+                    images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :], axis=(1, 2))
+            elif mode == 'avg':
+                output[:, i, j, :] = np.mean(
+                    images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :], axis=(1, 2))
+
+    return output
