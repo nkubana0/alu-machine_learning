000425858e777c5c9ad70e4a13a05ee73653a14f
nkubana0 <i.shema@alustudent.com>
2025-03-08 16:02:33 +0200

    convolve grayscale valid

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def convolve_grayscale_valid(images, kernel):
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    new_h, new_w = h - kh + 1, w - kw + 1
+    output = np.zeros((m, new_h, new_w))
+
+    for i in range(new_h):
+        for j in range(new_w):
+            output[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+    
+    return output
