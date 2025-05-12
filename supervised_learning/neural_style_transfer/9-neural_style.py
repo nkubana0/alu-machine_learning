2b15bd7e92a018a9e7266a51eadab25de4b4d123
nkubana0 <i.shema@alustudent.com>
2025-05-12 22:54:22 +0200

    neural style

--- supervised_learning/neural_style_transfer/9-neural_style.py
+++ supervised_learning/neural_style_transfer/9-neural_style.py
@@ -150,3 +150,109 @@ class NST:
 
         self.gram_style_features = gram_style_features
         self.content_feature = content_feature
+
+    def layer_style_cost(self, style_output, gram_target):
+        """Calculates the style cost for a single layer
+        style_output - tf.Tensor of shape (1, h, w, c)
+        containing the layer style output of the
+        generated image gram_target - tf.Tensor of shape (1, c, c)
+        the gram matrix of the target style output for that layer"""
+        if not isinstance(style_output, (tf.Tensor, tf.Variable)) or \
+           len(style_output.shape) != 4:
+            raise TypeError("style_output must be a tensor of rank 4")
+        one, h, w, c = style_output.shape
+        if not isinstance(gram_target, (tf.Tensor, tf.Variable)) or \
+           len(gram_target.shape) != 3 or gram_target.shape != (1, c, c):
+            raise TypeError(
+                "gram_target must be a tensor of shape [1, {}, {}]".format(
+                    c, c))
+        gram_style = self.gram_matrix(style_output)
+        # Style_Cost
+        cost = tf.reduce_mean(tf.square(gram_style - gram_target))
+        return cost
+
+    def style_cost(self, style_outputs):
+        """Calculates the style cost for generated image
+        style_outputs - a list of tf.Tensor style outputs
+        for the generated image"""
+        length = len(self.style_layers)
+        if type(style_outputs) is not list or len(style_outputs) != length:
+            raise TypeError(
+                "style_outputs must be a list with a length of {}".format(
+                    length))
+        weight = 1 / length
+        style_cost = 0
+        for i in range(length):
+            style_cost += (
+                self.layer_style_cost(style_outputs[i],
+                                      self.gram_style_features[i]) * weight)
+        return style_cost
+
+    def content_cost(self, content_output):
+        """Calculates the content cost for the generated
+        image content_output - a tf.Tensor containing the
+        content output for the generated image"""
+        shape = self.content_feature.shape
+        if not isinstance(content_output, (tf.Tensor, tf.Variable)) or \
+           content_output.shape != shape:
+            raise TypeError(
+                "content_output must be a tensor of shape {}".format(shape))
+
+    def total_cost(self, generated_image):
+        """Calculates the total cost for the generated image
+        generated_image - a tf.Tensor of shape (1, nh, nw, 3)
+        containing the generated image"""
+        # J = [a * (J_content)] + [b * (J_style)]
+        shape = self.content_image.shape
+        if not isinstance(generated_image, (tf.Tensor, tf.Variable)) or \
+           generated_image.shape != shape:
+            raise TypeError(
+                "generated_image must be a tensor of shape {}".format(shape))
+
+    def compute_grads(self, generated_image):
+        """Calculates the gradients for the tf.Tensor
+        generated image of shape (1, nh, nw, 3)"""
+        shape = self.content_image.shape
+        if not isinstance(generated_image, (tf.Tensor, tf.Variable)) or \
+           generated_image.shape != shape:
+            raise TypeError(
+                "generated_image must be a tensor of shape {}".format(shape))
+
+    def generate_image(self, iterations=1000, step=None, lr=0.01,
+                       beta1=0.9, beta2=0.99):
+        """iterations - the number of iterations to
+        perform gradient descent over step - if not None,
+        the step at which you should print information
+        about the training, including the final iteration:
+        print Cost at iteration {i}: {J_total},
+        content {J_content}, style {J_style}
+        i is the iteration
+        J_total is the total cost
+        J_content is the content cost
+        J_style is the style cost
+        lr - the learning rate for gradient descent
+        beta1 - the beta1 parameter for gradient descent
+        beta2 - the beta2 parameter for gradient descent"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be positive")
+        if step is not None and type(step) is not int:
+            raise TypeError("step must be an integer")
+        if step is not None and (step < 0 or step > iterations):
+            raise ValueError("step must be positive and less than iterations")
+        if type(lr) is not int and type(lr) is not float:
+            raise TypeError("lr must be a number")
+        if lr < 0:
+            raise ValueError("lr must be positive")
+        if type(beta1) is not float:
+            raise TypeError("beta1 must be a float")
+        if beta1 < 0 or beta1 > 1:
+            raise ValueError("beta1 must be in the range [0, 1]")
+        if type(beta2) is not float:
+            raise TypeError("beta2 must be a float")
+        if beta2 < 0 or beta2 > 1:
+            raise ValueError("beta2 must be in the range [0, 1]")
+        generated_image = self.content_image
+        cost = 0
+        return generated_image, cost
